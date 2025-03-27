901326ded796b6c795044d1674d728996ef00b54
Andrew Heiss <andrew@andrewheiss.com>
2025-03-27 17:46:26 -0400

    Better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 REMOTE_HOST="ath-cloud"
 REMOTE_DIR="~/sites/stats.andrewheiss.com/public_html/purple-parrot"
 REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
@@ -5,3 +7,14 @@ REMOTE_DEST=$REMOTE_HOST:$REMOTE_DIR
 echo "Uploading new changes to remote server..."
 echo
 rsync -crvP --exclude '*_cache' --delete _output/ $REMOTE_DEST
+
+if [ $? -eq 0 ]; then
+  echo
+  echo "... done!"
+  echo
+  echo "The site is live at https://stats.andrewheiss.com/purple-parrot/"
+else
+  echo
+  echo "Error!"
+  exit 1
+fi
